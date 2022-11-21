1c7255b6519e1e569f53ef7863413400d97b1add
Rakib099 <rakibnabi97@gmail.com>
2022-11-21 13:34:08 +0600

    Navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../images/logo.png';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    let activeClassName = "underline";
+    const style = {
+        color: "#f46969"
+    }
+
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar className='p-3' bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand className='fw-bold' style={style} href="#">
           <img
               alt=""
-              src="/logo.svg"
+              src={logo}
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
+            />{''}
+            uiz Wizard
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="statistics">Statistics</NavLink>
-            <NavLink className="nav-link" to="blog">Blog</NavLink>
+          <Nav className="ms-auto">
+            <NavLink style={style} className="nav-link fw-bold" to="/">Home</NavLink>
+            <NavLink style={style} className="nav-link fw-bold" to="statistics">Statistics</NavLink>
+            <NavLink style={style} className="nav-link fw-bold" to="blog">Blog</NavLink>
           </Nav>
         </Container>
       </Navbar>
