f8d7f0de6e93d0f10133301e6ee8e7640b70f7f4
Rakib099 <rakibnabi97@gmail.com>
2022-11-22 16:17:13 +0600

    chart added to statistics route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 import logo from '../../images/logo.png';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     const style = {
@@ -12,16 +12,18 @@ const Header = () => {
     return (
         <Navbar className='p-3 nav-bar' variant="light">
         <Container>
-          <Navbar.Brand className='fw-bold' style={style} href="/">
-          <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{''}
-            uiz Wizard
-          </Navbar.Brand>
+          <Link className='brand' to="/">
+            <Navbar.Brand className='fw-bold' style={style} >
+            <img
+                alt=""
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{''}
+              uiz Wizard
+            </Navbar.Brand>
+          </Link>
           <Nav className="ms-auto">
             <NavLink style={style} className="nav-link fw-bold" to="/">Home</NavLink>
             <NavLink style={style} className="nav-link fw-bold" to="statistics">Statistics</NavLink>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Statistics.css';
+import { 
+    Bar, 
+    BarChart, 
+    CartesianGrid, 
+    Legend, 
+    Line, 
+    LineChart, 
+    ResponsiveContainer, 
+    Tooltip, 
+    XAxis, 
+    YAxis 
+} from 'recharts';
 
 const Statistics = () => {
+    const topicData = useLoaderData();
+    const topic = topicData.data;
+    console.log(topic);
+
     return (
-        <div>
-            <h2>Statistics</h2>
-            <p>Chart</p>
-        </div>
+        <Container className='my-4'>
+            <h2 className='chart-title mb-5'>Total Number of Quiz for each topic shown in Charts</h2>
+            <Row className="chart-container">
+                <Col xs={12} lg={6}>
+                    <ResponsiveContainer width="100%" height={400}>
+                        <BarChart width={500} height={400} data={topic}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Bar dataKey="total" fill="#8884d8" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </Col>
+                <Col xs={12} lg={6}>
+                    <ResponsiveContainer width="100%" height={400}>
+                        <LineChart width={500} height={400} data={topic}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
