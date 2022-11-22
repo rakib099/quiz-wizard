faa63bdfd862d8e6d95288c8b6cf0f93e93a7145
Rakib099 <rakibnabi97@gmail.com>
2022-11-22 19:07:51 +0600

    error-page designed

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Error.css';
 import { Link, } from 'react-router-dom';
 
 const Error = () => {
   return (
     <div id="error" className="text-center mt-4">
-      <h1>Oops!</h1>
+      <h1 className='error-title'>Oops!</h1>
       <p>You are trying to access a page that doesn't exist on this website.</p>
       <p>
         <span>404 </span>
         <i>Not Found</i>
       </p>
-      <span>Return</span> to <Link to="/">Home</Link>
+      <span>Return to</span> <Link className='return' to="/">Home</Link>
     </div>
   );
 };
